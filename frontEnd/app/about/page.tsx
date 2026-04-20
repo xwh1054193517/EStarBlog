c31e7735032f10d65c00fbb2d1d97617f091dc05
wenhao <1054193517@qq.com>
2026-04-21 00:11:28 +0800

    fix: 部署解决环境变量

--- frontEnd/app/about/page.tsx
+++ frontEnd/app/about/page.tsx
@@ -1,6 +1,6 @@
 import { getMockSiteData } from "@/lib/mock-api";
 import AboutPageClient from "./AboutPageClient";
-
+export const dynamic = "force-dynamic";
 export default async function AboutPage() {
   const siteData = await getMockSiteData();
 
